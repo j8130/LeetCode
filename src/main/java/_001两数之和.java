f22b1612f4d5fa7217d37f1230e03ab32dfe69af
j8130 <37970263+j8130@users.noreply.github.com>
2019-10-23 08:50:57 +0800

    Update _001两数之和.java

--- src/main/java/_001两数之和.java
+++ src/main/java/_001两数之和.java
@@ -21,7 +21,21 @@
  */
 class Solution001 {
     public int[] twoSum(int[] nums, int target) {
-        //测试加一行数据
+        
+        int[] nums = [2, 7, 11, 15]; 
+        int[] target = 9;
+        int[] resoult;
+        for(int i = 0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+            
+                if((nums[i]+nums[j])==target){
+                
+                    resoult
+                
+                }
+            }
+        
+        }
         return null;
     }
 }
